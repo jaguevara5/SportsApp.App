db88e52a6571120fb0c648d2798894031c06b903
Alan Guevara <j.alanguevara@gmail.com>
2018-01-29 22:43:49 -0600

    Fixed games component layout

--- src/app/services/game-data/game-data.service.ts
+++ src/app/services/game-data/game-data.service.ts
@@ -4,23 +4,11 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class GameDataService {
 
-  private gamesDataListSource = new BehaviorSubject<any[]>([]);
-  private gameDataIdSource = new BehaviorSubject<string>('0');
   private gameDateIdSource = new BehaviorSubject<number>(1);
 
-  currentGameDataId = this.gameDataIdSource.asObservable();
-  currentGamesList = this.gamesDataListSource.asObservable();
   currentgameDateId = this.gameDateIdSource.asObservable();
 
   constructor() { }
-
-  updateGameDataId(gameId: string) {
-    this.gameDataIdSource.next(gameId);
-  }
-
-  updateGamesList(gamesList: any[]) {
-    this.gamesDataListSource.next(gamesList);
-  }
 
   updateGameDateId(gameDateId: number) {
     this.gameDateIdSource.next(gameDateId);
